9ef9bf83ec71ca925014fe6ead33f556d0b097cc
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 15:51:07 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,16 +1,11 @@
-<?php
-//Conexion con autenticacion de windows
-$serverName="mssql4.gear.host"; //serverName\instanceName
 
-// Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
-// La conexión se intentará utilizando la autenticación Windows.
-$connectionInfo=array("Database"=>"Celulares","UID"=>"celular","PWD"=>"@123456789");//"UID"=>"usuario","PWD"=>"contraseña"
-$conn=sqlsrv_connect($serverName,$connectionInfo); //Se estable la conexion
+<?php	
 
-if($conn) {
-    // echo "Conexión establecida.<br />";
-}else{
-     echo"Conexión no se pudo establecer.<br/>";
-     die( print_r( sqlsrv_errors(), true));
-}
+$usuario='celular';
+$pass='@123456789';
+$servidor='mssql4.gear.host';
+$basedatos='bdcelulares';
+
+$info=array('Datebase'=&gt;$basedatos,'UID'=&gt,$usuario,'PWD'=&gt;$pass);
+$conexion=sqlsrv_connect($servidor,$info);
 ?>
