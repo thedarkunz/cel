4a521b66adefc39ae0051963dd91ff397873a28b
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 09:13:24 -0500

    2

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,12 +1,19 @@
 
+<?php
 
 
- <?php
-$conn = sqlsrv_connect('ftp.gear.host','celularalmacen\$celularalmacen','4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe','celularalmacen\$celularalmacen');
-if(!conn){
-echo"mal";
 
+
+$serverName = "ftp.gear.host";
+
+
+$connectionInfo = array( "Database"=>"celularalmacen\$celularalmacen","UID"=>"celularalmacen\$celularalmacen","PWD"=>"4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe");//"UID"=>"usuario","PWD"=>"contraseña"
+$conn = sqlsrv_connect( $serverName, $connectionInfo); 
+
+if( $conn ) {
+    // echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
 }
-echo "bien";
-		
 ?>
