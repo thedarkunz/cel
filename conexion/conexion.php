3d8cb96fe7a4c9ce31a979b41a3e71a99b29e261
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 09:11:54 -0500

    3

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -2,7 +2,7 @@
 
 
  <?php
-$conn = sqlsrv_connect('ftp.gear.host','celularalmacen \ $ celularalmacen','4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe','celularalmacen \ $ celularalmacen');
+$conn = sqlsrv_connect('ftp.gear.host','celularalmacen\$celularalmacen','4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe','celularalmacen\$celularalmacen');
 if(!conn){
 echo"mal";
 
