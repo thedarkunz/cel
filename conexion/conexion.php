4fbe62e4f2a53e35538a36ff7f0c078cf873845c
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 15:25:50 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,5 +1,16 @@
 <?php
-	$conn = new PDO('sqlsrv:server=mssql4.gear.host;Database=bdcelulares', 'celular', '@123456789');
-	//$conn = new PDO("odbc:Driver={SQL Native Client};Server=mssql4.gear.host;Database=bdcelulares; Uid=celular;Pwd=@123456789;");
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//Conexion con autenticacion de windows
+$serverName = "BAILON-PC"; //serverName\instanceName
+
+// Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
+// La conexión se intentará utilizando la autenticación Windows.
+$connectionInfo = array( "Database"=>"Celulares","UID"=>"celular","PWD"=>"@123456789");//"UID"=>"usuario","PWD"=>"contraseña"
+$conn = sqlsrv_connect( $serverName, $connectionInfo); //Se estable la conexion
+
+if( $conn ) {
+    // echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
 ?>
