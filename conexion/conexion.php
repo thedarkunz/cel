1387aa3494fc34d7cc07973675b5d1372894b96b
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 14:56:45 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,10 +1,5 @@
 <?php
-	
-	/* uso de driver pdo con binarios de microsoft descargado desde su pagina web*/
-	$conn = new PDO('sqlsrv:server=mssql?.gear.host;Database=bdcelulares', 'celularalmacen\$celularalmacen','4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe');
-	
-
-	
-	/* atributos de funcionamiento de la libreria PDO*/
+	$conn = new PDO('sqlsrv:server=mssql4.gear.host;Database=bdcelulares', 'celular', '@123456789');
+	//$conn = new PDO("odbc:Driver={SQL Native Client};Server=mssql4.gear.host;Database=bdcelulares; Uid=celular;Pwd=@123456789;");
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
