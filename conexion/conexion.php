563702cd770e60c5a09088d9ee12c341cbefdc42
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 08:57:23 -0500

    conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,10 +1,19 @@
 <?php
-ini_set('display_errors','On');
 
-define("DB_NAME","celularalmacen\$celularalmacen");
-define("DB_USURNAME","celularalmacen\$celularalmacen");
-define("DB_PASSWORD","4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe");
 
-define("DB_HOST","ftp.gear.host");
 
+
+$serverName = "ftp.gear.host"; 
+
+
+
+$connectionInfo = array( "Database"=>"celularalmacen\$celularalmacen","UID"=>"celularalmacen\$celularalmacen","PWD"=>"4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe");//"UID"=>"usuario","PWD"=>"contraseña"
+$conn = sqlsrv_connect( $serverName, $connectionInfo); 
+
+if( $conn ) {
+  
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
 ?>
