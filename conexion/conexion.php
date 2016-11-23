a674ad17d2fedc1964825524501e7e0c733c88d6
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 15:30:15 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,6 +1,6 @@
 <?php
 //Conexion con autenticacion de windows
-$serverName = "BAILON-PC"; //serverName\instanceName
+$serverName = "ftp.gear.host"; //serverName\instanceName
 
 // Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
 // La conexión se intentará utilizando la autenticación Windows.
