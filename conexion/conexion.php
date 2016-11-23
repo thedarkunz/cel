ca0f6322ee08c9ec9fa9f5b41081197813427176
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-23 14:33:22 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,19 +1,10 @@
-
 <?php
+	
+	/* uso de driver pdo con binarios de microsoft descargado desde su pagina web*/
+	$conn = new PDO('sqlsrv:server=mssql?.gear.host;Database=bdcelulares', 'celularalmacen\$celularalmacen','4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe');
+	
 
-
-
-
-$serverName = "ftp.gear.host";
-
-
-$connectionInfo = array( "Database"=>"celularalmacen\$celularalmacen","UID"=>"celularalmacen\$celularalmacen","PWD"=>"4iSP0Eskcp18QdrdgM3fxXirCbqSkXv4ujTWYn96z1HYcjpeHrRmN5RooDwe");//"UID"=>"usuario","PWD"=>"contraseña"
-$conn = sqlsrv_connect( $serverName, $connectionInfo); 
-
-if( $conn ) {
-    // echo "Conexión establecida.<br />";
-}else{
-     echo "Conexión no se pudo establecer.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}
+	
+	/* atributos de funcionamiento de la libreria PDO*/
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
