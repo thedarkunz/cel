e84e9ea941278d4dcc6d62b321cf8e06c8088123
thedarkunz <losgatos_ko_142@hotmail.com>
2016-11-24 23:29:24 -0500

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -4,11 +4,11 @@ $serverName = "mssql4.gear.host"; //serverName\instanceName
 
 // Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
 // La conexión se intentará utilizando la autenticación Windows.
-$connectionInfo = array( "Database"=>"bdcelulare","UID"=>"bdcelulare","PWD"=>"Fo0S8V99-A7~");//"UID"=>"usuario","PWD"=>"contraseña"
+$connectionInfo = array( "Database"=>"bdcelulares","UID"=>"bdcelulares","PWD"=>"@123456789");//"UID"=>"usuario","PWD"=>"contraseña"
 //$conn = sqlsrv_connect( $serverName, $connectionInfo); //Se estable la conexion
 
 /* NUEVA CONEXION*/
-$conn = new PDO('sqlsrv:server=mssql4.gear.host;Database=bdcelulare', 'bdcelulare', 'Fo0S8V99-A7~');
+$conn = new PDO('sqlsrv:server=mssql4.gear.host;Database=bdcelulares', 'bdcelulares', '@123456789');
 //$conn = new PDO("odbc:Driver={SQL Native Client};Server=mssql4.gear.host;Database=bdcelulares; Uid=bdcelulares;Pwd=@123456789;");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -44,4 +44,3 @@ function executeQuery($conex,$SQLquery){
 }
 
 ?>
-
